c495ea388ffb5ad31c92a3a01aa47a32c88621b1
tflesch20 <33706294+tflesch20@users.noreply.github.com>
2019-06-26 08:22:31 -0400

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, HostListener } from '@angular/core';
 import { ConfigService, QA_LINK, RULES_LINK, DB_LINK, NG_LINK, DPAP_LINK } from './services/config';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { // implements OnDestroy {
 
-  //rendering: boolean;
+  rendering: boolean;
   expanded: boolean;
   config: any;
   qaURL: string;
@@ -16,6 +17,10 @@ export class AppComponent {
   dbAdminToolsURL: string;
   ngLink: string;
   dpapLink: string;
+  subscription: Subscription;
+  @HostListener('window:beforeunload', ['$event']) unloadHandler(event: Event) {
+    this.rendering = false;
+  }
 
   constructor(private configService: ConfigService,
               @Inject(QA_LINK) qaURL: string,
@@ -29,15 +34,14 @@ export class AppComponent {
     this.dbAdminToolsURL = dbAdminToolsURL;
     this.ngLink = ngLink;
     this.dpapLink = dpapLink;
-    ///this.rendering = false;
+    this.rendering = false;
     this.expanded = false;
   }
 
   inProgress(buttonPressed: string) {
-    //this.rendering = true;
-    if (buttonPressed === 'QA UI') { window.location.href = this.qaURL; }
-    if (buttonPressed === 'Rules UI') { window.location.href = this.rulesURL; }
-    if (buttonPressed === 'DBAdminTool UI') { window.location.href = this.dbAdminToolsURL; }
+    if (buttonPressed === 'QA UI') { window.location.href = this.qaURL; this.rendering = true; }
+    if (buttonPressed === 'Rules UI') { window.location.href = this.rulesURL; this.rendering = true; }
+    if (buttonPressed === 'DBAdminTool UI') { window.location.href = this.dbAdminToolsURL; this.rendering = true; }
   }
   NGHome() { window.open(this.ngLink); }
   DPaCHome() { window.open(this.dpapLink); }
